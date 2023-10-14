551540aca9020010bc699cd82ffe36df0d8bce23
Alaa Alhazmi <alaaalhazmi-is@hotmail.com>
2023-10-15 01:07:40 +0300

    Update_class_top_n_words _bertopic_variable.py

--- bertopic/_bertopic.py
+++ bertopic/_bertopic.py
@@ -804,7 +804,8 @@ class BERTopic:
     def topics_per_class(self,
                          docs: List[str],
                          classes: Union[List[int], List[str]],
-                         global_tuning: bool = True) -> pd.DataFrame:
+                         global_tuning: bool = True,
+                        class_top_n_words: int=10) -> pd.DataFrame:
         """ Create topics per class
 
         To create the topics per class, BERTopic needs to be already fitted once.
@@ -865,7 +866,7 @@ class BERTopic:
 
             # Fill dataframe with results
             topics_at_class = [(topic,
-                                ", ".join([words[0] for words in values][:20]),
+                                ", ".join([words[0] for words in values][:class_top_n_words]),
                                 topic_frequency[topic],
                                 class_) for topic, values in words_per_topic.items()]
             topics_per_class.extend(topics_at_class)
